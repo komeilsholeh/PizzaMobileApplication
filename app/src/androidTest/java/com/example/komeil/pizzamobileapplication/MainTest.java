b11aca3d298fa648f33375f0122d996d4368a7a5
komeilsholeh <21224612@student.uwl.ac.uk>
2015-12-12 01:57:16 +0000

    working on main.java get the selected item from spinner

---
+++ app/src/androidTest/java/com/example/komeil/pizzamobileapplication/MainTest.java
@@ -0,0 +1,10 @@
+package com.example.komeil.pizzamobileapplication;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Komeil on 12/12/2015.
+ */
+public class MainTest extends TestCase {
+
+}
--- app/src/main/java/com/example/komeil/pizzamobileapplication/Main.java
+++ app/src/main/java/com/example/komeil/pizzamobileapplication/Main.java
@@ -5,12 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
 
 public class Main extends AppCompatActivity {
 
+
+    private ArrayAdapter<String> arrayAdapter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,5 +54,17 @@ public class Main extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addTolist(View view){
+        Spinner pizzaTopping= (Spinner) findViewById(R.id.sprToppings);
+        ListView lstToppingList= (ListView) findViewById(R.id.lstToppings);
+
+        // getting the selected item from spinner
+        String topping=String.valueOf(pizzaTopping.getSelectedItem());
+
+        //
+
+
     }
 }
