00519c03d2c06e0a8d29bb09796f8cc2c7736a4e
komeilsholeh <21224612@student.uwl.ac.uk>
2015-12-13 00:20:59 +0000

    some changes in interface design

--- app/src/main/java/com/example/komeil/pizzamobileapplication/Main.java
+++ app/src/main/java/com/example/komeil/pizzamobileapplication/Main.java
@@ -2,7 +2,6 @@ package com.example.komeil.pizzamobileapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,8 +27,8 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      //  setSupportActionBar(toolbar);
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
