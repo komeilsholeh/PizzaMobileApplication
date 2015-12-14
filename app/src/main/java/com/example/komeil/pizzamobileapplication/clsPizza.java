10386d09a3b09f4b0f7813483e46cd8e479f4f03
komeilsholeh <21224612@student.uwl.ac.uk>
2015-12-14 23:10:33 +0000

    i am changing whole the program. i am making a new class for pizza and i should reprogram most of the parts.

---
+++ app/src/main/java/com/example/komeil/pizzamobileapplication/clsPizza.java
@@ -0,0 +1,28 @@
+package com.example.komeil.pizzamobileapplication;
+
+/**
+ * Created by Komeil on 14/12/2015.
+ */
+public class clsPizza {
+    private String pizzaName;
+    private String toppings;
+    private double price10;
+    private double price12;
+    private double price15;
+    private double price18;
+
+    public static final Pizza[] pizzas={
+        new Pizza("Margherita","Cheese,Tomato Sauce",7,10,14,17),new Pizza("Hawaiian","Cheese,Tomato Sauce,Ham,Pineapple",10,14,17,20),
+        new Pizza("Meat Feast","Cheese,Tomato Sauce,Pepperoni,Beef,Ham,Bacon",10,14,17,20),new Pizza("Pepperoni","2x Cheese,Tomato Sauce,2x Pepperoni",10,14,17,20),
+            new Pizza("Chicken Special","Cheese,Tomato Sauce,Chicken,Green Pepper,Red Onion,Sweet Corn",10,14,17,20)
+    };
+
+    public Pizza(String pizzaName,String toppings,double price10,double price12,double price15,double price18){
+        this.pizzaName=pizzaName;
+        this.toppings=toppings;
+        this.price10=price10;
+        this.price12 = price12;
+        this.price15=price15;
+        this.price18=price18;
+    }
+}
